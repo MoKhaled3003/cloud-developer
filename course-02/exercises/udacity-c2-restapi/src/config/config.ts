c8cc61cddd8593d664326ea917e5ee41439457c9
unknown <mo.khaled3003@gmail.com>
2020-02-23 10:23:23 +0200

    old

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "modevudacity",
+    "password": "modevudacity",
+    "database": "modevudacity",
+    "host": "mo-dev-udacity.cywk87gh76md.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": "us-east-1",
+    "aws_profile": "mo-dev",
+    "aws_media_bucket": "mo-udagram-dev"
   },
   "prod": {
     "username": "",
